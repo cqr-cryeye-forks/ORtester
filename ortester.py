cea159fd861c8a0ed95731769885ad8a320efe48
Evil2997 <dima.molchanow2012@gmail.com>
2024-05-17 16:26:21 +0300

    Removed unnecessary lines of code without using arguments. Changed various codes and names of some functions.

--- ortester.py
+++ ortester.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python
+import json
 import os
+import pathlib
+from typing import Final
+
 import requests
 import tldextract
 import signal
 import warnings
 from optparse import OptionParser
-from colorama import *
-
-
-def banner():
-    print(
-        Style.BRIGHT
-        + Fore.YELLOW
-        + """
-           ___   ___   __                   __
-          ||  | ||  | || |_    ___   ____  || |_    ___   __ __
-          ||  | ||  | ||  _|  //  \ // __| ||  _|  //  \ || '__|
-          ||  | ||=-  || |_  ||---- \__  \ || |_  ||---- || |
-          ||__| ||  |  \___|  \____ ||___/  \___|  \____ ||_|
-
-                                 [ Leon (geek_pwn@protonmail.com) ]
-                                          [ twitter.com/leonmugen ]
-        """
-        + Style.RESET_ALL
-    )
+import urllib3
 
 
 def main():
-    os.system("clear")
-    warnings.filterwarnings("ignore")
-    banner()
-    usage = "Usage: python %prog [-h] -u 'URL' -f [file]"
-
-    parser = OptionParser(usage=usage)
-    parser.add_option("-u", "--url", dest="url", help="target URL")
-    parser.add_option("-f", "--file", dest="file", help="payloads file")
-
-    (options, args) = parser.parse_args()
+    all_data = []
     if options.url is None:
         parser.print_help()
         exit()
 
-    # Payloads file
-    if options.file is True:
-        s.addOption("file", True)
-
-    # Replace string in payloads file
-    urlD = options.url
-    info = tldextract.extract(urlD)
+    info = tldextract.extract(URL)
     domain_name = info.registered_domain
     payloadlist = open("payloads.list", encoding="latin-1").readlines()
     newlist = open("all.list", "w")
@@ -60,7 +31,7 @@ def main():
     newlist.close()
 
     # Open file
-    with open(options.file) as f:
+    with open(file_all_list) as f:
         for payload in f:
             payloadF = payload.strip()
             urlF = options.url + payloadF
@@ -69,42 +40,37 @@ def main():
             # Get the response.
             try:
                 response = requests.get(urlF, verify=False)
-            except requests.exceptions.ConnectionError:
-                print("No server response")
-                exit()
-
-            # ===Process to find an open redirect===.
-            if response.history:
-                # Compare the destination url with Bing's url.
-                if (
-                    str(response.url)[0:19] == "http://www.bing.com"
-                    or str(response.url)[0:20] == "https://www.bing.com"
-                ):
-
-                    print(
-                        Style.BRIGHT
-                        + Fore.YELLOW
-                        + "Open Redirect Vulnerability found!"
-                        + Style.RESET_ALL
-                    )
-                    print(Fore.YELLOW + "Redirected to: " + response.url)
-                    print(
-                        Style.BRIGHT
-                        + Fore.BLUE
-                        + "Payload ---> "
-                        + payloadF
-                        + Style.RESET_ALL
-                    )
-                    exit()
+                # ===Process to find an open redirect===.
+                if response.history:
+                    # Compare the destination url with Bing's url.
+                    if (
+                            str(response.url)[0:19] == "http://www.bing.com"
+                            or str(response.url)[0:20] == "https://www.bing.com"
+                    ):
+                        print("Open Redirect Vulnerability found!")
+                        print("Redirected to: " + response.url)
+                        print("Payload ---> " + payloadF)
+                        data_find = {
+                            "msg": "Open Redirect Vulnerability found",
+                            "Redirected_to": response.url,
+                            "Payload": payloadF,
+                        }
+                        all_data.append(data_find)
+                    else:
+                        print("Redirected to: " + response.url)
+                        data_find = {
+                            "Redirected_to": response.url
+                        }
+                        all_data.append(data_find)
                 else:
-                    print(
-                        Fore.YELLOW + "Redirected to: " + response.url + Style.RESET_ALL
-                    )
-
-            else:
-                print(
-                    "Request was not redirected. Check manually because it might be a redirect using javascript. \n"
-                )
+                    print("Request was not redirected. Check manually because it might be a redirect using javascript. \n")
+            except requests.exceptions.ConnectionError:
+                print("ConnectionError")
+            except urllib3.exceptions.LocationParseError:
+                print("LocationParseError")
+            except requests.exceptions.InvalidURL:
+                print("InvalidURL")
+        return all_data
 
 
 # Press ctrl+c to finish
@@ -113,10 +79,31 @@ def ctrl_c(signum, rfm):
     exit()
 
 
-signal.signal(signal.SIGINT, ctrl_c)
+if __name__ == '__main__':
+    os.system("clear")
+    warnings.filterwarnings("ignore")
 
-try:
-    main()
-    print(Fore.YELLOW + "RESULT: " + Style.RESET_ALL + "No Open Redirect Found!")
-except (TypeError):
-    print("Usage: python ortester.py -u 'URL' -f [file]\n")
+    file_all_list: Final[pathlib.Path] = pathlib.Path(__file__).parent / "all.list"
+    usage = "Usage: python %prog [-h] -u 'URL' -f [file]"
+
+    parser = OptionParser(usage=usage)
+    parser.add_option("--url", dest="url", help="target URL")
+    parser.add_option("--output", help="output save in json")
+
+    (options, args) = parser.parse_args()
+    URL: str = options.url
+    output: str = options.output
+
+    MAIN_DIR: Final[pathlib.Path] = pathlib.Path(__file__).parent
+    output_json: Final[pathlib.Path] = MAIN_DIR / output
+
+    signal.signal(signal.SIGINT, ctrl_c)
+    
+    ALL_DATA = main()
+    
+    if ALL_DATA == []:
+        ALL_DATA = {
+            "RESULT": "No Open Redirect Found!"
+        }
+    with open(output_json, "w") as jf:
+        json.dump(ALL_DATA, jf, indent=2)
